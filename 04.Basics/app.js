587a7650374de97662d101084aedeb13ac25352a
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-03 00:32:37 +0530

    relation operator in js

---
+++ 04.Basics/app.js
@@ -0,0 +1,10 @@
+//Boolean - true or false
+let isTrue= true;
+console.log(isTrue);
+//question and answer
+let isJsProgammingLanguage=true;
+let isJsHard= false;
+let favNumb=21;
+console.log(isJsProgammingLanguage)
+console.log(isJsHard);
+console.log(favNumb+undefined);
---
+++ 04.Basics/app2.js
@@ -0,0 +1,7 @@
+//Camparison Operators
+// Relational Operators
+console.log(10>10);//false
+console.log(10<10);//false
+console.log(10>=10); //true
+console.log(10<=10);//true
+console.log(10==10);//true
