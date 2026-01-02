24ca607eb129e0cb340ddfa7a59c16bc4d851e5a
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-03 00:22:47 +0530

    this is the file of maths operation

--- 02.Basics/app.js
+++ 02.Basics/app.js
@@ -3,3 +3,6 @@ let name="gaurav jain";//assig value
 let num= 5;
 console.log(name);
 console.log(num);
+let gaurav10= "variable";
+gaurav10=7;
+console.log(gaurav10);
---
+++ 03.Basics/app1.js
@@ -0,0 +1,16 @@
+let num=2
+//Basic Math operations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let counter =0;
+counter++;
+counter++;
+counter++;
+counter--;
+counter++;
+console.log(counter);
