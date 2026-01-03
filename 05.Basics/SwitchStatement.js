f3bbeb1c4ddb92147e3fa7316d40deeceaefbe40
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-03 21:21:55 +0530

    this is my switch case file

---
+++ 05.Basics/SwitchStatement.js
@@ -0,0 +1,14 @@
+let x='0';
+switch(x){
+    case '0':
+        console.log("x is zero");
+        break;
+    case '1':
+        console.log("x is one");
+        break;
+    case '2':
+        console.log("x is two");
+        break;
+    default:
+        console.log("x is neither zero nore one no wtoo");            
+}
