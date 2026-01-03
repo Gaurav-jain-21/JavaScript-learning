9268f81044bfe44aa0f4505ec004a73469fd87dd
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-03 23:24:02 +0530

    initial commit

---
+++ 06.Basics/logicalOperator.js
@@ -0,0 +1,12 @@
+// 1. -> Logical AND (&&)
+//-> True if both the operands /boolean values are true, else false
+const a = true;
+const b= false;
+const c= 4;
+console.log(a&&b);
+// 2. -> Logical OR (||)
+//-> TRUE if either of the operands/ boolean values is true. evalutes to false if both are false.
+console.log(a||b);
+// 3. -> Logical NOT (!)
+//-> True if the operands if false and vice-versa
+console.log(!a);
