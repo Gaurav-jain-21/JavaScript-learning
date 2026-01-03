042905a494ccfb9f9aa134930a4f84881cb8198f
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-03 21:06:10 +0530

    this is converstion file

---
+++ 05.Basics/typeconvertion.js
@@ -0,0 +1,13 @@
+let money="50";
+money=Number(money);
+console.log(typeof money);
+// convert number to string
+
+console.log("converting number to string");
+let price =100;
+price= price.toString();
+console.log(typeof price);
+console.log("converting string into float");
+let cost= "63.54";
+cost= parseFloat(cost);
+console.log(typeof cost);
