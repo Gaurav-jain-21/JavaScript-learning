0c1bdd8375c70cab9eb48a1f3d449a36a76f8e01
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-04 20:56:15 +0530

    this is the file

--- nodejs1/3.Path/index.js
+++ nodejs1/3.Path/index.js
@@ -1,3 +1,10 @@
 import path from "path";
 console.log(path.basename("c:\\nodejs\\index.js"));
-console.log(path.basename("c\\nodejs\\app.js",".js"));
+console.log(path.basename("c:\\nodejs\\app.js",".js"));
+console.log(path.dirname("c:\\nodejs\\app.js"));
+console.log(path.extname("c:\\nodejs\\app.js"));
+console.log(path.join("c:","gaurav-jain","coures","redux-toolkit"));
+
+
+
+
