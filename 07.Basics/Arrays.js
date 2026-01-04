c0612909e1a19d612422f4985a88d450047dba56
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-04 13:35:28 +0530

    this is file

--- 07.Basics/Arrays.js
+++ 07.Basics/Arrays.js
@@ -10,3 +10,9 @@ console.log(stringArr[3]);
 
 const nestArr=['one',["two","three"],1,true,false];
 console.log(nestArr[1][0]);
+
+const pl=["javaScript","goLang", "python", "php"]
+const number =[3,4,5,1,2]
+console.log(pl.join("-"));
+console.log(pl.reverse());
+console.log(pl.slice(0,1));
