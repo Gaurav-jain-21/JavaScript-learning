94a3749b06d96ad05a0dfd35507e6458929c3db3
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-04 13:52:02 +0530

    this is file

---
+++ 08.Basics/function1.js
@@ -0,0 +1,14 @@
+function greet(){
+    console.log("hello world");
+}
+
+function sayHello(name){
+    console.log(`hello ${name}`);
+}
+// sayHello("gaurav");
+// sayHello("ankit");
+
+function add(x,y){
+    return x+y;
+}
+console.log(add(10,20));
