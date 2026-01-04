9e1629e429a984d36eea4c8b0e61377f1bcb792f
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-04 20:48:36 +0530

    this is my file

---
+++ nodejs1/2.Impoets/index.js
@@ -0,0 +1,7 @@
+import greet from "./greet.js";
+import {person1, person2, person3} from "./peoples.js";
+console.log(person1);
+console.log(person2);
+console.log(person3);
+greet("gaurav");
+greet("Apple");
---
+++ nodejs1/2.Impoets/peoples.js
@@ -0,0 +1,4 @@
+let person1="ram";
+let person2="gaurav";
+let person3="vikash";
+export {person1, person2, person3};
