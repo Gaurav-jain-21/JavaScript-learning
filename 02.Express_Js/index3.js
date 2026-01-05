c4612e6bc430e6dc8935d13237eee19cef2ce38f
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-05 23:37:22 +0530

    this is my page

--- 02.Express_Js/index3.js
+++ 02.Express_Js/index3.js
@@ -1,5 +1,7 @@
 import express from "express";
 const app=express();
 app.get("/product/iphone",(req,res)=>{
-    
+    res.send("this code will only run if you provide /product/iphone");
+
 });
+app.listen(8000,()=>console.log("server up"));
