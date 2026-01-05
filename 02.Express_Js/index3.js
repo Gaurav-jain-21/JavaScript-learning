8276691c07b53c43d4e9616e75455852412ec174
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-06 00:02:21 +0530

    this is my file

--- 02.Express_Js/index3.js
+++ 02.Express_Js/index3.js
@@ -1,7 +1,24 @@
 import express from "express";
-const app=express();
-app.get("/product/iphone",(req,res)=>{
-    res.send("this code will only run if you provide /product/iphone");
 
+const app = express();
+
+app.get("/product/iphone", (req, res) => {
+    res.send("this code will only run if you provide /product/iphone");
 });
-app.listen(8000,()=>console.log("server up"));
+
+app.get("/single-cb", (req, res) => {
+    res.send("Single Callback");
+});
+
+app.get(
+    "/double-cb",
+    (req, res, next) => {
+        console.log("first callback");
+        next(); // move to next callback
+    },
+    (req, res) => {
+        res.send("we dont talk");
+    }
+);
+
+app.listen(8000, () => console.log("server up"));
