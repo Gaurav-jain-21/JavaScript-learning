9c7b0ed01d7b0c732593b1664e0fa95b2de3e36b
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-08 23:49:02 +0530

    this is my page

---
+++ demo_Project/app.js
@@ -0,0 +1,2 @@
+const button =document.querySelector("button")
+button.addEvenListener("click",()=>console.log("you clicked me"));
---
+++ demo_Project/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.listen(8000, () => console.log("Server running on port 8000"));
